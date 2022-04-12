e890d1c3d75a62ba375a4484d6787fef9481d972
Aleksei <skalatskyalexey@gmail.com>
2022-04-13 01:08:54 +0700

    Broadcast [sc-4674] - flexbox grid of image previews

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,10 +1,13 @@
 // Packages
 import React from 'react';
 import PropTypes from 'prop-types';
-import ImageGallery from 'react-photo-gallery';
+// import ImageGallery from 'react-photo-gallery';
+
+// Child components
+import { ImagePreview } from '@cway/cway-frontend-common/components';
 
 // Constants
-import { previewSizeEnum } from '@cway/cway-frontend-common/constants';
+// import { previewSizeEnum } from '@cway/cway-frontend-common/constants';
 
 // Helpers
 import { getPreviewUrl } from '@cway/cway-frontend-common/utils';
@@ -15,34 +18,47 @@ const styles = {
   root: {
     flex: 1,
     overflowY: 'auto',
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignContent: 'start',
+  },
+  imageWrapper: {
+    flex: 0,
+    padding: 5,
   },
 };
 
-const photos = [
-  {
-    src: 'http://example.com/example/img1.jpg',
-    width: 4,
-    height: 3
-  },
-  {
-    src: 'http://example.com/example/img2.jpg',
-    width: 1,
-    height: 1
-  }
-];
 const Gallery = ({ classes, fileIds }) => {
+  // return (
+  //   <div className={classes.root}>
+  //     {fileIds.map((id) => <img key={id} height="200" src={getPreviewUrl({ id })} alt="" />)}
+  //   </div>
+  // );
+
+  // return (
+  //   <div className={classes.root}>
+  //     {fileIds.map((id) => <p key={id}>{id}</p>)}
+  //   </div>
+  // );
+
+  // const images = fileIds.map((id) => ({ src: getPreviewUrl({ id }), width: 1, height: 1 }));
+  // return (
+  //   <ImageGallery photos={images} />
+  // );
+
+  const previewSizeLimits = { width: 150, height: 150 };
   return (
     <div className={classes.root}>
-      {fileIds.map((id) => <img key={id} height="200" src={getPreviewUrl({ id, page: 1, size: previewSizeEnum.small })} alt="" />)}
+      {fileIds.map((id) => (
+        <div className={classes.imageWrapper} key={id}>
+          <ImagePreview
+            url={getPreviewUrl({ id })}
+            maxWidth={previewSizeLimits.width}
+            maxHeight={previewSizeLimits.height}
+          />
+        </div>
+      ))}
     </div>
-  );
-  return (
-    <div className={classes.root}>
-      {fileIds.map((id) => <p key={id}>{id}</p>)}
-    </div>
-  );
-  return (
-    <ImageGallery photos={photos} />
   );
 };
 
