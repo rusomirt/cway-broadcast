11989653169d7842b7f8ea46a7bb406ec2e4c358
Aleksei <skalatskyalexey@gmail.com>
2022-04-12 11:49:25 +0700

    Broadcast [sc-4674] - mock broadcast backend data

--- src/components/LeftPanel/index.jsx
+++ src/components/LeftPanel/index.jsx
@@ -20,6 +20,61 @@ const styles = {
   },
 };
 
+// const BROADCAST = loader('@cway/cway-frontend-common/graphql/public/queries/Broadcast.graphql');
+const broadcastMock = {
+  id: 'uuid_broadcast',
+  name: 'bc1',
+  type: 'FOLDER',
+  fileDescriptor: [
+    {
+      __typename: 'FDFolder',
+      id: 'folder1uuid',
+      name: 'folder1',
+    },
+    {
+      __typename: 'FDFolder',
+      id: 'folder2uuid',
+      name: 'folder2',
+    },
+    {
+      __typename: 'FDFile',
+      id: 'file11uuid',
+      name: 'file11',
+      parent: 'folder1uuid',
+    },
+    {
+      __typename: 'FDFile',
+      id: 'file12uuid',
+      name: 'file12',
+      parent: 'folder1uuid',
+    },
+    {
+      __typename: 'FDFile',
+      id: 'file21uuid',
+      name: 'file21',
+      parent: 'folder2uuid',
+    },
+    {
+      __typename: 'FDFolder',
+      id: 'folder13uuid',
+      name: 'folder13',
+      parent: 'folder1uuid',
+    },
+    {
+      __typename: 'FDFile',
+      id: 'file131uuid',
+      name: 'file131',
+      parent: 'folder13uuid',
+    },
+    {
+      __typename: 'FDFolder',
+      id: 'folder14uuid',
+      name: 'folder14',
+      parent: 'folder1uuid',
+    },
+  ],
+};
+
 const LeftPanel = ({ classes }) => {
   return (
     <div className={classes.root}>
