945386c01c8a9172ff4758b83f1730d31d3d2a2b
Aleksei <skalatskyalexey@gmail.com>
2022-04-13 00:01:32 +0700

    Broadcast [sc-4674] - show files previews in rows

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ImageGallery from 'react-photo-gallery';
 
+// Constants
+import { previewSizeEnum } from '@cway/cway-frontend-common/constants';
+
+// Helpers
+import { getPreviewUrl } from '@cway/cway-frontend-common/utils';
+
 // Styling
 import withStyles from '@material-ui/core/styles/withStyles';
 const styles = {
@@ -27,7 +33,12 @@ const photos = [
 const Gallery = ({ classes, fileIds }) => {
   return (
     <div className={classes.root}>
-      {fileIds.map((id) => <p>{id}</p>)}
+      {fileIds.map((id) => <img key={id} height="200" src={getPreviewUrl({ id, page: 1, size: previewSizeEnum.small })} alt="" />)}
+    </div>
+  );
+  return (
+    <div className={classes.root}>
+      {fileIds.map((id) => <p key={id}>{id}</p>)}
     </div>
   );
   return (
